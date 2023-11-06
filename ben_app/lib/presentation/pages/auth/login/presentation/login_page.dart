3df52327a87338d513c522f31da0053abe8e82d4
Paul <paul.julienne@theTribe.io>
2023-11-06 21:37:32 +0100

    add hello + deploy

--- ben_app/lib/presentation/pages/auth/login/presentation/login_page.dart
+++ ben_app/lib/presentation/pages/auth/login/presentation/login_page.dart
@@ -7,7 +7,7 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('login'),
+        child: Text('Hello ben :)'),
       ),
     );
   }
